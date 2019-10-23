1080a6c6735b08a47015d561e8941abf703f60f8
Jan Mares <janxmares@gmail.com>
2019-10-23 18:18:07 +0200

    finalizing code to adjust ILO mean wage

--- 181203 Union density from ILO.R
+++ 181203 Union density from ILO.R
@@ -11,7 +11,7 @@ library(Rilostat)
 
 
 # Set the working directory
-wd <- c("c:/Users/JM/Dropbox/Research/Finance and income inequality/")
+wd <- c("c:/Users/JM/Documents/GitHub/finineq/")
 setwd(wd)
 
 a <- get_ilostat_toc(search="union")
@@ -82,13 +82,116 @@ data_meanwage <- data_meanwage[!(ref_area == "CAN" & !(source %in% c("BA:147")))
 # chile
 data_meanwage <- data_meanwage[!(ref_area == "CHL" & !(source %in% c("DA:134","BX:138"))),]
 
+# costa rica
+data_meanwage <- data_meanwage[!(ref_area == "CRI" & !(source %in% c("BX:142"))),]
 
+# cyprus
+data_meanwage <- data_meanwage[!(ref_area == "CYP" & !(source %in% c("FA:38"))),]
 
+# czech republic
+data_meanwage <- data_meanwage[!(ref_area == "CZE" & !(source %in% c("DA:1050","DA:293"))),]
 
+# germany
+data_meanwage <- data_meanwage[!(ref_area == "DEU" & !(source %in% c("BB:304"))),]
 
+# egypt
+data_meanwage <- data_meanwage[!(ref_area == "EGY" & !(source %in% c("BA:582","CA:1162"))),]
 
+# spain
+data_meanwage <- data_meanwage[!(ref_area == "ESP" & !(source %in% c("DA:248","FA:1131"))),]
 
-View(data_meanwage)
+# greece
+data_meanwage <- data_meanwage[!(ref_area == "GRC" & !(source %in% c("BA:317"))),]
+
+# guatemala
+data_meanwage <- data_meanwage[!(ref_area == "GTM" & !(source %in% c("FA:1203","BA:660"))),]
+
+# hungary
+data_meanwage <- data_meanwage[!(ref_area == "HUN" & !(source %in% c("CA:1237","DA:3089"))),]
+
+# ireland
+data_meanwage <- data_meanwage[!(ref_area == "IRL" & !(source %in% c("DA:410"))),]
+
+# cambodia
+data_meanwage <- data_meanwage[!(ref_area == "KHM" & !(source %in% c("BB:3064","BA:1321"))),]
+
+# lithuania
+data_meanwage <- data_meanwage[!(ref_area == "LTU" & !(source %in% c("DA:12956","EA:6396"))),]
+
+# luxembourg
+data_meanwage <- data_meanwage[!(ref_area == "LUX" & !(source %in% c("FA:189"))),]
+
+# magagascar
+data_meanwage <- data_meanwage[!(ref_area == "MDG" & !(source %in% c("BA:7423"))),]
+
+# mexico
+data_meanwage <- data_meanwage[!(ref_area == "MEX" & !(source %in% c("BA:463"))),]
+
+# macedonia
+data_meanwage <- data_meanwage[!(ref_area == "MKD" & !(source %in% c("DA:3100","DA:2601"))),]
+
+# mauritius
+data_meanwage <- data_meanwage[!(ref_area == "MUS" & !(source %in% c("BA:82","DA:1442"))),]
+
+# malaysia
+data_meanwage <- data_meanwage[!(ref_area == "MYS" & !(source %in% c("DA:7341"))),]
+
+# norway
+data_meanwage <- data_meanwage[!(ref_area == "NOR" & !(source %in% c("DA:287"))),]
+
+# peru
+data_meanwage <- data_meanwage[!(ref_area == "PER" & !(source %in% c("BX:375"))),]
+
+# philipines
+data_meanwage <- data_meanwage[!(ref_area == "PHL" & !(source %in% c("BA:629","DA:1539"))),]
+
+# poland
+data_meanwage <- data_meanwage[!(ref_area == "POL" & !(source %in% c("DA:428"))),]
+
+# puerto rico
+data_meanwage <- data_meanwage[!(ref_area == "PRT" & !(source %in% c("BA:199"))),]
+
+# singapur
+data_meanwage <- data_meanwage[!(ref_area == "SGP" & !(source %in% c("BA:5873","FA:5921"))),]
+
+# serbia
+data_meanwage <- data_meanwage[!(ref_area == "SRB" & !(source %in% c("BA:467","DA:3105"))),]
+
+# slovakia
+data_meanwage <- data_meanwage[!(ref_area == "SVK" & !(source %in% c("DA:399"))),]
+
+# slovenia
+data_meanwage <- data_meanwage[!(ref_area == "SVN" & !(source %in% c("DA:5911","FX433"))),]
+
+# sweden
+data_meanwage <- data_meanwage[!(ref_area == "SWE" & !(source %in% c("DA:3477","DA:1665"))),]
+
+# thailand
+data_meanwage <- data_meanwage[!(ref_area == "THA" & !(source %in% c("BA:741"))),]
+
+# tajikistan
+data_meanwage <- data_meanwage[!(ref_area == "TJK" & !(source %in% c("CA:1705"))),]
+
+# tanzania
+data_meanwage <- data_meanwage[!(ref_area == "TZA" & !(source %in% c("BA:1743","DA:2543"))),]
+
+# south africa
+data_meanwage <- data_meanwage[!(ref_area == "ZAF" & !(source %in% c("DA:1811","BA:595"))),]
+
+# order mean wage by country, year
+data_meanwage <- data_meanwage[order(ref_area, year),]
+
+# write it into excel
+# write.csv(data_meanwage, file="mw.csv", row.names = F)
+
+#
+#
+
+# read the manually adjusted data in excel
+data_mw <- data.table(read.csv(file="ILO_meanwage_final.csv", header = T, stringsAsFactors = T))
+
+#
+#
 
 # checking the duplicit values
 
